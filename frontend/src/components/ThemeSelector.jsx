4be09f4825922a2d2be3133b4b1c571d5cdbc105
userzenox <piyush.132kp@gmail.com>
2025-08-25 19:09:44 +0530

    commit-done

--- frontend/src/components/ThemeSelector.jsx
+++ frontend/src/components/ThemeSelector.jsx
@@ -5,7 +5,7 @@ import { useThemeStore } from "../store/useThemeStore";
 function ThemeSelector() {
   const { theme, setTheme } = useThemeStore();
   
-   console.log(theme);
+  //  console.log(theme);
    
   return (
     <div className="dropdown dropdown-end">
